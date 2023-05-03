8c8e52c219164a18cb901c6d4f3d692b6c2fcb1a
Jhow Rodrigues <jhonatta_rodrigues@hotmail.com>
2023-05-03 10:36:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// está dizendo ao Express que irá usar o EJS como View Engine
+app.set('view engine','ejs')
+
+//Para a aplicação aceitar arquivos estáticos, no exemplo vindo da pasta public 
+app.use(express.static('public'))
+
+// render irá renderizar o html de acordo com o arquivo criado na view
+app.get('/',(req, res)=>{
+    
+    res.render("index")
+})
+
+app.listen(8080,()=>{
+    console.log('Aplicação ON!')
+})
