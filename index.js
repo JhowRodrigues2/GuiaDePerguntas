7452550f5acab8d1f2fb9c0d601d4090ff6b5e66
Jhow Rodrigues <jhonatta_rodrigues@hotmail.com>
2023-05-05 07:43:39 -0300

    install dependences sequilize mysql

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
 
 // está dizendo ao Express que irá usar o EJS como View Engine
 app.set('view engine','ejs')
@@ -7,15 +9,24 @@ app.set('view engine','ejs')
 //Para a aplicação aceitar arquivos estáticos, no exemplo vindo da pasta public 
 app.use(express.static('public'))
 
+//permite que a pessoa envie os dados do formulário e bodyparser irá traduzir pra javascript
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 // render irá renderizar o html de acordo com o arquivo criado na view
 app.get('/',(req, res)=>{
-    
     res.render("index")
 })
 app.get('/perguntar',(req, res)=>{
     
     res.render("perguntar")
 })
+app.post('/salvarpergunta',(req, res)=>{
+    let titulo= req.body.titulo
+    let descricao= req.body.descricao
+    res.send(titulo)
+})
+
 app.listen(8080,()=>{
     console.log('Aplicação ON!')
 })
